67c11df93f5dc2e6bfce838789f92a2af92b0b5d
Raphael-Sammy <133025924+Raphael-Sammy@users.noreply.github.com>
2024-08-28 09:44:57 -0300

    Add files via upload
    Aqui está a prática de consultas com mais de uma tabela ao mesmo tempo.

---
+++ SQLQuery6_Raphael.sql
@@ -0,0 +1,56 @@
+select * from Estadias e, Hospedes h 
+where	e.dataEntrada between '01-01-2018'
+and '31-12-2018' and h.CPF = e.Hospede;
+
+select * from Servicos s, Solicitacoes so
+where	so.data between '01-09-2023'
+and '30-09-2023' and s.codServico = so.servico;
+
+select  nome, hora from Hospedes h, Servicos s, Solicitacoes so
+where s.descricao in ('Lavandeira','Babá') and h.CPF = so.hospede and so.servico = s.codServico;
+
+
+select hospede, servico
+From Hospedes h, Servicos s, Solicitacoes so
+where h.sexo = 'M' and h.CPF = so.hospede and s.codServico = so.servico;
+
+
+
+select h.nome, s.descricao
+from Hospedes h, Servicos s, Solicitacoes so
+where h.CPF = so.hospede and s.codServico = so.servico and so.data between '01-01-2012' and '30-06-2012'
+
+
+select s.*, h.*
+from Hospedes h, Servicos s, Solicitacoes so
+where h.nome like '%Santos%' and h.CPF = so.hospede and so.servico = s.codServico;
+
+
+select h.nome, e.dataEntrada 
+from Hospedes h, Estadias e, Quartos q
+where q.tipo in ('Standard','Luxo superior') and e.dataEntrada between '01-01-2012' and '31-12-2012'
+and h.CPF = e.hospede and e.quarto = q.numero
+
+select e.dataEntrada, e.dataSaida
+from Hospedes h, Estadias e
+where h.CPF = e.hospede 
+order by e.dataEntrada DESC;
+
+
+
+select h.nome, so.data, so.hora
+from Hospedes h, Servicos s, Solicitacoes so
+where h.sexo = 'F' and h.CPF = so.hospede and s.codServico = so.servico;
+
+
+
+select distinct h.nome
+from Hospedes h, Estadias e, Servicos s, Solicitacoes so, Quartos q
+where h.CPF = e.hospede and q.numero = e.quarto and s.codServico = so.servico and h.CPF =  so.hospede
+and q.valorDiaria > '500' and s.descricao in('Lanchonete','Passadeira');
+
+
+
+
+select * from Solicitacoes
+
